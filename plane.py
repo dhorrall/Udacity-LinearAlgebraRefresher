d02f1a6d8fea7e97572ce0d6ccebebc6acc563b9
Derek Horrall <derekh1@oc7043351046.ibm.com>
2017-01-09 06:49:47 -0500

    From upstairs

--- plane.py
+++ plane.py
@@ -1,6 +1,7 @@
 from decimal import Decimal, getcontext
 
 from vector import Vector
+import sys
 
 getcontext().prec = 30
 
@@ -33,7 +34,7 @@ class Plane(object):
             initial_index = Plane.first_nonzero_index(n)
             initial_coefficient = n[initial_index]
 
-            basepoint_coords[initial_index] = c/initial_coefficient
+            basepoint_coords[initial_index] = c/Decimal(initial_coefficient)
             self.basepoint = Vector(basepoint_coords)
 
         except Exception as e:
@@ -42,13 +43,54 @@ class Plane(object):
             else:
                 raise e
 
-
+    def find_intersection(self,aLine):
+        
+        if not self.is_parallel(aLine):
+            #print(self.normal_vector.coordinates)
+            A=Decimal(self.normal_vector.coordinates[0])
+            B=Decimal(self.normal_vector.coordinates[1])
+            C=Decimal(aLine.normal_vector.coordinates[0])
+            D=Decimal(aLine.normal_vector.coordinates[1])
+            K1=self.constant_term
+            K2=aLine.constant_term
+            X= (D*K1-B*K2)/(A*D-B*C)
+            Y= -(C*K1-A*K2)/(A*D-B*C)
+            result = 'X= '+str(round(X,3))    ,'Y= ' + str(round(Y,3))
+            return result
+        elif self == aLine:
+            result = 'Lines are the same line!'
+            return result
+        else:
+            result = 'Lines are parallel, but not the same!, no intersection!'
+            return result
+    
+    def is_parallel(self,aPlane):
+        r1 = self.normal_vector
+        r2 = aPlane.normal_vector
+        result = r1.is_parallel(r2)
+        #print (result)
+        return result
+    
+    def __eq__(self,aPlane):
+        if not self.is_parallel(aPlane):
+            return False
+        #compute vector connecting the lines base points
+        x0 = self.basepoint
+        y0 = aPlane.basepoint
+        #print (type(x0),type(y0))
+        basepoint_diff = x0.subtract(y0)
+        n = self.normal_vector
+        #print(n)
+        #if connecting vector is orthogonal then lines are equal
+        return basepoint_diff.is_orthogonal(n)
+    
     def __str__(self):
 
         num_decimal_places = 3
-
+        
         def write_coefficient(coefficient, is_initial_term=False):
             coefficient = round(coefficient, num_decimal_places)
+           
             if coefficient % 1 == 0:
                 coefficient = int(coefficient)
 
@@ -68,26 +110,28 @@ class Plane(object):
             return output
 
         n = self.normal_vector
-
+        print (type(n))
         try:
+            
             initial_index = Plane.first_nonzero_index(n)
             terms = [write_coefficient(n[i], is_initial_term=(i==initial_index)) + 'x_{}'.format(i+1)
-                     for i in range(self.dimension) if round(n[i], num_decimal_places) != 0]
+                     for i in range(self.dimension) if round((n[i]), num_decimal_places) != 0]
             output = ' '.join(terms)
 
         except Exception as e:
             if str(e) == self.NO_NONZERO_ELTS_FOUND_MSG:
                 output = '0'
             else:
+                print('test',sys.exc_traceback.tb_lineno)                
                 raise e
-
+        #sys.exit(0)
         constant = round(self.constant_term, num_decimal_places)
         if constant % 1 == 0:
             constant = int(constant)
         output += ' = {}'.format(constant)
 
         return output
-
+    
 
     @staticmethod
     def first_nonzero_index(iterable):
